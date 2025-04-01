f67e5305151913bbb2f302672683d9ebc993a4d6
paulineonier <pauline.onier@outlook.fr>
2025-04-01 10:30:49 +0200

    correction task 1, number of line

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module qui extrait les 3e et 4e colonnes d'une matrice.
-"""
+"""Module qui extrait les 3e et 4e colonnes d'une matrice."""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = [[row[2], row[3]] for row in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
