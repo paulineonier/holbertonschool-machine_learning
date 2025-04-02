5e399c245d609e7ab5569410e43ddd594ce6e590
paulineonier <pauline.onier@outlook.fr>
2025-04-02 11:59:26 +0200

    task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module: array_concatenation
+This module contains a function to concatenate two lists of integers or floats.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists of integers or floats.
+
+    Args:
+        arr1 (list): The first list containing integers or floats.
+        arr2 (list): The second list containing integers or floats.
+
+    Returns:
+        list : list containing elements of arr1 followed by elements of arr2.
+    """
+    return arr1 + arr2
