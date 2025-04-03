14ad5eef13c2846f16f2a58f960638dc5cef15d0
paulineonier <pauline.onier@outlook.fr>
2025-04-03 10:43:12 +0200

    pycodestyle correction, task 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
 Module: matrix_multiplication
-This module provides a function to perform matrix multiplication for 2D matrices.
+Provides a function to perform matrix multiplication for 2D matrices.
 """
+
 
 def mat_mul(mat1, mat2):
     """
@@ -10,7 +11,7 @@ def mat_mul(mat1, mat2):
 
     Args:
         mat1 (list of list): The first 2D matrix containing integers or floats.
-        mat2 (list of list): The second 2D matrix containing integers or floats.
+        mat2 (list of list): Second 2D matrix containing integers or floats.
 
     Returns:
         list of list: A new matrix resulting from multiplying mat1 and mat2.
@@ -19,7 +20,7 @@ def mat_mul(mat1, mat2):
     # Check if multiplication is possible: columns in mat1 == rows in mat2
     if len(mat1[0]) != len(mat2):
         return None
-    
+
     # Initialize the resulting matrix with zero values
     result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
@@ -28,5 +29,5 @@ def mat_mul(mat1, mat2):
         for j in range(len(mat2[0])):  # Iterate through columns of mat2
             for k in range(len(mat2)):  # Iterate through rows of mat2
                 result[i][j] += mat1[i][k] * mat2[k][j]
-    
+
     return result
