a4d126ca1690cd965df37710fe03d11db8bfd3e8
paulineonier <pauline.onier@outlook.fr>
2025-04-03 11:23:31 +0200

    task 10, correction, code without import

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 """
-This module provides a function to calculate the shape of a numpy.ndarray.
+This module provides a function to calculate the shape of numpy.ndarray.
 """
-
-import numpy as np
 
 
 def np_shape(matrix):
     """
-    Calculates the shape of a numpy.ndarray.
+    Calculates the shape of numpy.ndarray.
 
     Args:
-        matrix (numpy.ndarray): Input array whose shape is to be determined.
+        matrix (numpy.ndarray): Input array, shape is to be determined.
 
     Returns:
-        tuple: A tuple of integers representing the dimensions of the array.
+        tuple: Tuple of integers representing dimensions of numpy.ndarray.
                For example:
-               - A 1D array returns (size,).
-               - A 2D array returns (rows, columns).
-               - A 3D array returns (depth, rows, columns), etc.
+               - A 1D list returns (size,).
+               - A 2D list returns (rows, columns).
+               - A 3D list returns (depth, rows, columns), etc.
     """
     return matrix.shape
