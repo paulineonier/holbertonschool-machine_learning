7155f89c3eee34d29426ae371666b2b1d439002f
paulineonier <pauline.onier@outlook.fr>
2025-04-08 10:14:14 +0200

    task 1 correction, changed the color and deleted marker

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -7,18 +7,18 @@ def scatter():
     """
     Plots a scatter plot of men's height vs weight with magenta points.
     """
+    np.random.seed(5)
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
-    np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
-    plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, c='m', marker='.')  # 'm' for magenta points
-    plt.xlabel('Height (in)')            # axis x
-    plt.ylabel('Weight (lbs)')           # axis y
-    plt.title("Men’s Height vs Weight")  # Title of the plot
-    plt.show()                           # Display the graphic
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, color='magenta')
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men’s Height vs Weight")
+    plt.show()
 
 
 scatter()
