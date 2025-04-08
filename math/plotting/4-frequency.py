22b6e7a2d5f0153f61bd123c04bf2a761f6a406c
paulineonier <pauline.onier@outlook.fr>
2025-04-08 13:05:56 +0200

    task 4, correction

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module plots a histogram of student scores for Project A.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,13 +10,24 @@ import matplotlib.pyplot as plt
 def frequency():
     """
     Plots a histogram of student scores for Project A.
+
+    The histogram displays the distribution of student grades, with:
+    - The x-axis labeled "Grades"
+    - The y-axis labeled "Number of Students"
+    - Bins set every 10 units, ranging from 0 to 100
+    - The bars outlined in black to make them clearly visible
+
+    Random distribution student grades generated using normal distribution
+    with a mean of 68 and a standard deviation of 15.
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
+    plt.figure(figsize=(6.4, 4.8))  # Set the figure size
 
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    # Plotting the histogram with bins every 10 units
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+
+    # Adding labels and title
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    plt.show()
