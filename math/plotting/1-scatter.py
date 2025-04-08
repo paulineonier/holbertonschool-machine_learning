b268a3d60aea39a3fb2ed96deec07fce7a9ac7b4
paulineonier <pauline.onier@outlook.fr>
2025-04-08 10:24:37 +0200

    task 1 correction, and changing the versions

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,22 +2,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def scatter():
     """
     Plots a scatter plot of men's height vs weight with magenta points.
     """
-    np.random.seed(5)
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
 
     plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, color='magenta')
-    plt.xlabel("Height (in)")
-    plt.ylabel("Weight (lbs)")
+    plt.scatter(x, y, c='m', marker='.')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
     plt.title("Men’s Height vs Weight")
+    plt.grid(True)
     plt.show()
 
 
