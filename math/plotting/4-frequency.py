c6746b8456ab901f0dac1519ef86fdbafa163d6b
paulineonier <pauline.onier@outlook.fr>
2025-04-10 10:49:05 +0200

    task 4, correction, adding ylim

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -17,14 +17,12 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # Plot histogram with bins every 10 units and black edges
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-
-    # Configure the graph
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.xticks(range(0, 101, 10))
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
     plt.show()
