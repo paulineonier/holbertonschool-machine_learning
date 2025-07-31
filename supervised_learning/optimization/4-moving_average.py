e627c950d89aadf1d92ef5a2521436154ac5926c
paulineSWE <pauline.onier@holbertonschool.com>
2025-07-31 15:11:37 +0200

    task 4, execution and pycodestyle ok

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Function that calculates the weighted moving average of a data set"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def moving_average(data, beta):
+    """
+    Calculates the weighted moving average of a data set.
+
+    Parameters:
+    - data: list of data to calculate the moving average
+    - beta: weight used for the moving average
+
+    Returns:
+    - list containing the moving average of data
+    """
+    v = 0
+    moving_averages = []
+
+    for t, x in enumerate(data, 1):
+        v = beta * v + (1 - beta) * x
+        v_corrected = v / (1 - beta ** t)
+        moving_averages.append(v_corrected)
+
+    return moving_averages
