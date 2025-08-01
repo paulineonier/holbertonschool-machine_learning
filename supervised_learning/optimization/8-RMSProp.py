240bf13b8bd6012064f6278bd85a0f5bf0b10c45
paulineSWE <pauline.onier@holbertonschool.com>
2025-08-01 10:21:14 +0200

    task 8, first try

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+RMSProp Optimizer Setup for TensorFlow
+"""
+
+import tensorflow as tf
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Creates the RMSProp optimizer
+
+    Parameters:
+    - alpha: learning rate
+    - beta2: RMSProp decay (discounting factor for the history of squared gradients)
+    - epsilon: small constant to avoid division by zero
+
+    Returns:
+    - optimizer: a tf.keras.optimizers.RMSprop instance
+    """
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
+    return optimizer
