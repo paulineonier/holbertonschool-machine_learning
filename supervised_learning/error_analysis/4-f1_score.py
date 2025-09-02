fd06bb871943a8c0519d7b4a4afb4736b5ea5df9
paulineSWE <pauline.onier@holbertonschool.com>
2025-09-02 12:25:32 +0200

    task 4, correction pycodestyle

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -9,6 +9,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
     """
     Calculates the F1 score for each class.
