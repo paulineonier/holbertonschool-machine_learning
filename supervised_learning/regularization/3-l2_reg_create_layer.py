c83c59963be0d075984176225d3c07ea83b8fff2
paulineSWE <pauline.onier@holbertonschool.com>
2025-09-10 11:30:36 +0200

    task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,44 +1,40 @@
 #!/usr/bin/env python3
 """
-Module 3-l2_reg_create_layer
-This module defines a function to create a Keras layer with L2
-regularization.
-
-Functions:
-    l2_reg_create_layer(prev, n, activation, lambtha):
-        Creates a dense layer with L2 regularization.
+Defines a function to create a layer with L2 regularization
 """
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a neural network layer in TensorFlow that includes
-    L2 regularization.
+    Creates a TensorFlow layer that includes L2 regularization.
 
     Args:
-        prev (tf.Tensor): Tensor containing the output of the previous
-                          layer.
-        n (int): Number of nodes in the new layer.
-        activation (callable): Activation function to use in the layer.
-        lambtha (float): L2 regularization parameter.
+        prev: tensor containing the input to the layer
+        n (int): number of nodes in the layer
+        activation: activation function to use
+        lambtha (float): L2 regularization parameter
 
     Returns:
-        tf.Tensor: The output tensor of the new layer.
+        Tensor output of the layer
     """
-    regularizer = None
-    if lambtha > 0:
-        regularizer = tf.keras.regularizers.L2(lambtha)
+    # L2 regularizer
+    l2_reg = tf.keras.regularizers.L2(lambtha)
 
-    layer = tf.keras.layers.Dense(
+    # He et al. initialization (variance scaling initializer)
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
+
+    # Dense layer with L2 regularization
+    layer = tf.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=regularizer,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(
-            scale=2.0,
-            mode='fan_avg'
-        )
+        kernel_initializer=initializer,
+        kernel_regularizer=l2_reg,
+        name="layer"
     )
 
     return layer(prev)
