af0be0b3e298726111b33631bc6e06f008cc4707
paulineSWE <pauline.onier@holbertonschool.com>
2026-03-20 15:53:14 +0100

    task 0 correction, incorrect output, second correction

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
+"""
+PCA module
+"""
 import numpy as np
+
 
 def pca(X, var=0.95):
     """
-    Performs PCA on dataset X
+    Performs PCA on a dataset.
+
+    Parameters:
+    X (numpy.ndarray): shape (n, d), centered dataset
+    var (float): fraction of variance to preserve
+
+    Returns:
+    numpy.ndarray: weight matrix W of shape (d, nd)
     """
+    n, d = X.shape
 
-    # SVD
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    # Compute covariance matrix
+    cov = np.matmul(X.T, X) / (n - 1)
 
-    # Explained variance ratio
-    explained_variance = (S ** 2) / np.sum(S ** 2)
+    # Eigen decomposition
+    eigenvalues, eigenvectors = np.linalg.eig(cov)
 
-    # Cumulative variance
+    # Sort eigenvalues and eigenvectors in descending order
+    idx = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[:, idx]
+
+    # Compute explained variance ratio
+    explained_variance = eigenvalues / np.sum(eigenvalues)
+
+    # Compute cumulative variance
     cumulative_variance = np.cumsum(explained_variance)
 
-    # Number of components
+    # Find nd
     nd = np.where(cumulative_variance >= var)[0][0] + 1
 
-    # Projection matrix
-    W = Vt.T[:, :nd]
+    # Compute W
+    W = eigenvectors[:, :nd]
 
     return W
