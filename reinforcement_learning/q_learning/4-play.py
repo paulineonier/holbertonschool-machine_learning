833110973b48f99299807758784f93c5a6bcd386
paulineSWE <pauline.onier@holbertonschool.com>
2026-09-22 11:46:35 +0200

    correction task 4

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -5,22 +5,21 @@ import numpy as np
 
 
 def play(env, Q, max_steps=100):
-    """Play an episode and return rewards and rendered outputs."""
-    state, _ = env.reset()
-    rendered_outputs = []
+    """Play an episode using the trained Q-table."""
+    state = env.s
     total_rewards = 0
-
-    rendered_outputs.append(env.render())
+    rendered_outputs = []
 
     for _ in range(max_steps):
+        rendered_outputs.append(env.render())
+
         action = np.argmax(Q[state])
 
         state, reward, terminated, truncated, _ = env.step(action)
-
         total_rewards += reward
-        rendered_outputs.append(env.render())
 
         if terminated or truncated:
+            rendered_outputs.append(env.render())
             break
 
     return total_rewards, rendered_outputs
