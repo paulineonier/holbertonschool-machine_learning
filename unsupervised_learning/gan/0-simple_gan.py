f6d12f5f233e5b93e3dd80d78757a7e9ee3cdad3
paulineSWE <pauline.onier@holbertonschool.com>
2026-09-22 16:53:18 +0200

    task 0, correction, adding docstring

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -1,13 +1,46 @@
 #!/usr/bin/env python3
+"""
+Module 0-simple_gan
+Contains the Simple_GAN class implementation for training a standard GAN.
+"""
+
 import tensorflow as tf
 from tensorflow import keras
 
 
 class Simple_GAN(keras.Model):
+    """
+    Simple Generative Adversarial Network (GAN) class.
+
+    Attributes:
+        latent_generator (function): Function generating latent vector samples.
+        real_examples (tf.Tensor): Tensor containing real training examples.
+        generator (keras.Model): Neural network generating fake samples.
+        discriminator (keras.Model): Neural network evaluating sample validity.
+        batch_size (int): Size of training batches.
+        disc_iter (int): Discriminator steps per generator step.
+        learning_rate (float): Optimizer learning rate.
+        beta_1 (float): Adam optimizer exponential decay rate for 1st moment.
+        beta_2 (float): Adam optimizer exponential decay rate for 2nd moment.
+    """
 
     def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
+        """
+        Initializes the Simple_GAN instance with loss functions and optimizers.
+
+        Args:
+            generator (keras.Model): The generator network.
+            discriminator (keras.Model): The discriminator network.
+            latent_generator (function): Function to sample latent vectors.
+            real_examples (tf.Tensor): Dataset of real training instances.
+            batch_size (int, optional): Size of training batch. Default 200.
+            disc_iter (int, optional): Discriminator updates per step.
+                                       Default 2.
+            learning_rate (float, optional): Optimizer learning rate.
+                                             Default 0.005.
+        """
         super().__init__()
         self.latent_generator = latent_generator
         self.real_examples = real_examples
@@ -50,6 +83,18 @@ class Simple_GAN(keras.Model):
         )
 
     def get_fake_sample(self, size=None, training=False):
+        """
+        Generates a batch of fake samples using the generator.
+
+        Args:
+            size (int, optional): Number of fake samples. Defaults to
+                self.batch_size.
+            training (bool, optional): Whether training mode is active.
+                Defaults to False.
+
+        Returns:
+            tf.Tensor: Generated fake samples.
+        """
         if not size:
             size = self.batch_size
         return self.generator(
@@ -58,6 +103,16 @@ class Simple_GAN(keras.Model):
         )
 
     def get_real_sample(self, size=None):
+        """
+        Extracts a random subset of real training samples.
+
+        Args:
+            size (int, optional): Number of real samples. Defaults to
+                self.batch_size.
+
+        Returns:
+            tf.Tensor: Random subset of real samples.
+        """
         if not size:
             size = self.batch_size
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
@@ -65,6 +120,15 @@ class Simple_GAN(keras.Model):
         return tf.gather(self.real_examples, random_indices)
 
     def train_step(self, useless_argument):
+        """
+        Executes one training step of the GAN (Discriminator + Generator).
+
+        Args:
+            useless_argument: Required placeholder by Keras fit API.
+
+        Returns:
+            dict: Dictionary containing 'discr_loss' and 'gen_loss'.
+        """
         for _ in range(self.disc_iter):
             with tf.GradientTape() as disc_tape:
                 real_samples = self.get_real_sample()
